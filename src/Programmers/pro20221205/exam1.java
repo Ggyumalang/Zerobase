6a12de668e654f4bf6941a2a20050535289abeaf
Ggyumalang <khg2154@naver.com>
2022-12-05 15:35:23 +0900

    20221205 commit

---
+++ src/Programmers/pro20221205/exam1.java
@@ -0,0 +1,27 @@
+package Programmers.pro20221205;
+
+import java.util.Arrays;
+
+public class exam1 {
+    static final int mod = 1234567;
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int[] dp = new int[n+1];
+        if(n <= 1){
+            return n;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-2]%mod + dp[i-1]%mod;
+        }
+
+        return dp[n]%mod;
+    }
+}
