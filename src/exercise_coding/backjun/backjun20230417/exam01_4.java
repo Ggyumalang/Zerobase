78e80524fd1db3653d0bce7287bc7ee43927bec2
Ggyumalang <khg2154@naver.com>
2023-04-17 11:59:40 +0900

    20230417 commit

--- src/exercise_coding/backjun/backjun20230417/exam01_4.java
+++ src/exercise_coding/backjun/backjun20230417/exam01_4.java
@@ -1,4 +1,3 @@
-//백준 - 1062 가르침
 package exercise_coding.backjun.backjun20230417;
 
 import java.io.BufferedReader;
@@ -10,59 +9,60 @@ import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class exam01_3 {
+public class exam01_4 {
 
+    static final int ALPHABET_LEN = 26;
     static int N,K;
-    static int maxVal;
     static String[] subString;
-    static final int ALPHABET = 26;
-    static boolean[] visited = new boolean[ALPHABET];
-
+    static boolean[] visited;
+    static int maxVal;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        subString = new String[N];
 
-        if(K < 5) { //a , n , t , i , c
+        if( K < 5 ){
             System.out.println(0);
-        }else if(K == 26) {
+        }else if( K >= 26){
             System.out.println(N);
-        } else {
+        }else {
+            subString = new String[N];
+
             for (int i = 0; i < N; i++) {
-                String str = br.readLine();
-                subString[i] = str.substring(4, str.length()-4);//앞에 anta , tica 제거
+                //앞에 4, 뒤에 4 제외하기
+                String s = br.readLine();
+                subString[i] = s.substring(4, s.length()-4);
             }
-            //a,n,t,i,c 방문 처리
+
+            visited = new boolean[ALPHABET_LEN];
+            //a n t i c
             visited[0] = true;
-            visited['n'-'a'] = true;
-            visited['t'-'a'] = true;
-            visited['i'-'a'] = true;
-            visited['c'-'a'] = true;
-            //조합 -> dfs
-            combination(0,0);
+            visited['n' - 'a'] = true;
+            visited['t' - 'a'] = true;
+            visited['i' - 'a'] = true;
+            visited['c' - 'a'] = true;
+
+            backtrack(0,0);
             System.out.println(maxVal);
         }
+        
     }
 
-    private static void combination(int idx, int start) {
+    private static void backtrack(int idx, int start) {
 
-        //총 단어 6개라면 a n t i c 를 제외 한 조합 1개
         if(idx == K-5){
             int count = 0;
-
             for (int i = 0; i < N; i++) {
-                boolean isValid = true;
-
+                boolean isPass = true;
                 for (int j = 0; j < subString[i].length(); j++) {
                     if(!visited[subString[i].charAt(j) - 'a']){
-                        isValid = false;
+                        isPass = false;
                         break;
                     }
                 }
-                if(isValid){
+                if(isPass){
                     count++;
                 }
             }
@@ -70,10 +70,10 @@ public class exam01_3 {
             return;
         }
 
-        for (int i = start; i < ALPHABET; i++) {
+        for (int i = start; i < ALPHABET_LEN; i++) {
             if(!visited[i]){
                 visited[i] = true;
-                combination(idx+1, i+1);
+                backtrack(idx+1, i+1);
                 visited[i] = false;
             }
         }
