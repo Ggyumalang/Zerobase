9c61ce179ac574edda6bbef99f5402efd18dbea2
Ggyumalang <khg2154@naver.com>
2023-05-25 15:45:36 +0900

    20230525 commit

---
+++ src/exercise_coding/backjun/backjun20230525/exam01.java
@@ -0,0 +1,33 @@
+//백준 - 타일 채우기
+//어려움.. 틀렸음 => 05.26 다시 풀이 예정
+//아직도 잘 이해가 안감.. 이런 건 직접 그려가면서 풀어가야하나 봄..
+package exercise_coding.backjun.backjun20230525;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class exam01 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N % 2 != 0){
+            System.out.println(0);
+            return;
+        }
+
+        int[] dp = new int[N+1];
+        int accumulate = 0;
+        dp[2] = 3;
+        for (int i = 4; i <= N; i+=2) {
+            accumulate += dp[i-4] * 2 ; //예외상황
+            dp[i] = dp[i-2] * 3 + 2 + accumulate;
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
---
+++ src/exercise_coding/backjun/backjun20230525/exam02.java
@@ -0,0 +1,51 @@
+//백준 - 전깃줄
+package exercise_coding.backjun.backjun20230525;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class exam02 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N];
+        Arrays.fill(dp,1);
+        int[][] connected = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            connected[i][0] = Integer.parseInt(st.nextToken());
+            connected[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(connected, (x,y) -> x[0] - y[0]);
+
+        for (int i = 0; i < N; i++) {
+            /*
+             * i번째 전봇대를 기준으로 이전의 전봇대들의
+             * 전선을 연결하기 위한 탐색
+             * 즉, i번째 전봇대에 연결된 B전봇대는
+             * 탐색할 j번째 전봇대에 연결된 B전봇대보다 값이 커야함
+             */
+
+            for (int j = 0; j < i; j++) {
+                if(connected[i][1] > connected[j][1]){
+                    dp[i] = Math.max(dp[i] , dp[j] + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(N - answer);
+    }
+}
