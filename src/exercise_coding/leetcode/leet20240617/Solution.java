7ecccbf6b4a4e878021905051b0c13bfb6a850a0
Ggyumalang <khg2154@naver.com>
2024-06-17 21:43:54 +0900

    20240617 - leetCode - Group Anagrams

---
+++ src/exercise_coding/leetcode/leet20240617/Solution.java
@@ -0,0 +1,31 @@
+//leetCode 49.Group Anagrams
+package exercise_coding.leetcode.leet20240617;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+
+            map.get(sortedStr).add(str);
+        }
+
+        System.out.println(map);
+
+        return new ArrayList<>(map.values());
+    }
+}
